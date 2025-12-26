70e886f01e161267193a1e470ab5aa2da82bc53c
gabriel <49964470+369gabriel@users.noreply.github.com>
2025-12-26 19:52:57 -0300

    icons goes from 0 to 500 :O

--- tiamat/Icons.py
+++ tiamat/Icons.py
@@ -1,25 +1,20 @@
 import json
 from termcolor import colored
-from Rengar import Rengar  # Certifique-se de que o Rengar.py está no mesmo diretório
+from Rengar import Rengar
 
 def change_profile_icon():
-    # Inicializa a classe Rengar
     rengar = Rengar()
 
-    # Pede o ID do ícone ao usuário
-    icon_id = input(colored("Type the icon ID (1 - 100): \n", "magenta"))
+    icon_id = input(colored("Type the icon ID (1 - 500): \n", "magenta"))
 
-    # Verifica se o input é um número válido
     try:
         icon_id = int(icon_id)
     except ValueError:
         print("Please insert a valid number.")
         return
 
-    # Cria o corpo da requisição
     body = {"profileIconId": icon_id}
 
-    # Faz a requisição PUT usando a classe Rengar
     try:
         response = rengar.lcu_request("PUT", "/lol-summoner/v1/current-summoner/icon", body)
         if response.status_code == 201 or response.status_code == 200:
